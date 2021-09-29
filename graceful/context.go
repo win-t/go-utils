cd9d51db141a4e305ac638b4fb1a5ac66fa83476
Kurnia D Win <kurnia.d.win@gmail.com>
2021-09-29 08:11:58 +0700

    update graceful

--- graceful/context.go
+++ graceful/context.go
@@ -13,21 +13,41 @@ import (
 
 var (
 	ctx         context.Context
+	cancelFn    context.CancelFunc = func() {}
+	wg          *sync.WaitGroup    = &sync.WaitGroup{}
 	contextOnce sync.Once
 )
 
 // Context for graceful shutdown
 func Context() context.Context {
 	contextOnce.Do(func() {
-		var cancelFn context.CancelFunc
 		ctx, cancelFn = context.WithCancel(context.Background())
 		go func() {
 			defer cancelFn()
 			c := make(chan os.Signal, 1)
 			signal.Notify(c, getInterruptSigs()...)
-			<-c
+			select {
+			case <-c:
+			case <-ctx.Done():
+			}
 			signal.Stop(c)
 		}()
 	})
 	return ctx
+}
+
+// Shutdown cancel the graceful context and wait until the wait counter is zero
+func Shutdown() {
+	cancelFn()
+	wg.Wait()
+}
+
+// WaitAdd increase the wait counter
+func WaitAdd() {
+	wg.Add(1)
+}
+
+// WaitDone decrease the wait counter
+func WaitDone() {
+	wg.Done()
 }
